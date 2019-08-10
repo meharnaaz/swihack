aa9501adf8d944c737ffba5fdddf253855f4f773
meharnaaz <41200386+meharnaaz@users.noreply.github.com>
2019-08-10 14:05:46 +0530

    Create swishack.c

---
+++ swishack.c
@@ -0,0 +1,17 @@
+# swihack
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int x,p,fsum=0;
+   scanf("%d%d",&x,&p);
+   p=100-p;
+   while(x>0)
+   {
+       fsum=fsum+x;
+       x=(p*x)/100;
+   }
+   printf("%d",fsum);
+   return 0;
+} 
+
